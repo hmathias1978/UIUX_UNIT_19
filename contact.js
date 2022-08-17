95ad6760f2ab05c0934e3b91d511a0c2a6de43e8
Heather <mathias.heatherm@gmail.com>
2022-08-17 16:47:11 -0700

    New Save
    New save under new username.

---
+++ contact.js
@@ -0,0 +1,18 @@
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+
+  <!-- Hotjar Tracking Code for https://matt-boisseau.github.io/hobby-prototype/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2153874,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
